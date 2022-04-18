12deb45a0ab4601283bef95d3024bc2c00d12b53
Wilhelm Liao <williaml.twcp@gmail.com>
2022-04-18 15:38:37 +0800

    first commit

---
+++ facade.go
@@ -0,0 +1,45 @@
+package esresponse
+
+import (
+	"io"
+)
+
+func AsSearchResult(responseBody io.Reader) (*SearchResult, error) {
+	var result = new(SearchResult)
+
+	err := decode(responseBody, result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func AsIndexResponse(responseBody io.Reader) (*IndexResponse, error) {
+	var result = new(IndexResponse)
+
+	err := decode(responseBody, result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func AsCountResult(responseBody io.Reader) (*CountResult, error) {
+	var result = new(CountResult)
+
+	err := decode(responseBody, result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func AsGetResult(responseBody io.Reader) (*GetResult, error) {
+	var result = new(GetResult)
+
+	err := decode(responseBody, result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/Bofry/elasticsearch-esresponse
+
+go 1.14
+
+require (
+	github.com/elastic/go-elasticsearch v0.0.0
+	github.com/elastic/go-elasticsearch/v7 v7.10.0
+	github.com/pkg/errors v0.9.1
+)
